e64bb9f78261290ed68ae2967d3fb5b52ea4d17e
akshat102 <48102705+akshat102@users.noreply.github.com>
2020-01-10 19:41:23 +0530

    Update ConnectionClass.java

--- ConnectionClass.java
+++ ConnectionClass.java
@@ -3,13 +3,14 @@ package ERROR;
 import java.sql.*;
 
 import javax.swing.JOptionPane;
+
 public class ConnectionClass {
 	public static Connection dbconnect() {
-		try {
+		try{
 			Class.forName("Oracle.jdbc.OracleDriver");
-			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "SYSTEM#2429");
+			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "system");
 			return conn;
-		} catch(Exception e) {
+		}catch(Exception e) {
 			JOptionPane.showMessageDialog(null, e);
 			return null;
 		}
